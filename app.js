175f0b3cae8900cbf135a313a230b5a6af2f9511
byref17 <124827023+byref17@users.noreply.github.com>
2023-04-10 14:17:20 +0200

    final

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 
 app.use(express.static("public"));
 
+
+app.get("/", (req, res, nex) => {
+    res.sendFile(__dirname + "/views/home.html");
+
+});
+
+app.get("/about", (req, res, nex) => {
+    res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/work", (req, res, nex) => {
+    res.sendFile(__dirname + "/views/work.html");
+});
+
+app.get("/gallery", (req, res, nex) => {
+    res.sendFile(__dirname + "/views/gallery.html");
+});
+
 app.listen(3000, () => {
     console.log("My first app listening on port 3000");
 });
-
-app.get("/", (request, response, next) => {
-    response.sendFile(__dirname + "/views/about.html");
-});
-
-app.get("/", (request, response, next) => {
-    response.sendFile(__dirname + "/views/galery.html");
-});
-
-app.get("/", (request, response, next) => {
-    response.sendFile(__dirname + "/views/home.html");
-});
-
-app.get("/", (request, response, next) => {
-    response.sendFile(__dirname + "/views/work.html");
-});
-
-// ********************************
